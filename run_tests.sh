cd50706b0949117f710b1b1e69cdabd5fcb6bc62
Seb Bacon <seb.bacon@gmail.com>
2020-03-21 14:17:43 +0000

    Exit on first test failure

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,4 @@ WARNING_FILTER="ignore:KernelManager._kernel_spec_manager_changed:DeprecationWar
 # This awkward testing of exit codes is to get around the case where
 # no tests are found, which has exit code of 5 in pytest, but we don't
 # want to treat as a failure
-PYTHONPATH=$(pwd) python -m pytest --sanitize-with config/nbval_sanitize_file.conf --nbval . -W $WARNING_FILTER; ret=$?; [ $ret = 5 ] && exit 0 || exit $ret
+PYTHONPATH=$(pwd) python -m pytest --exit-first --sanitize-with config/nbval_sanitize_file.conf --nbval . -W $WARNING_FILTER; ret=$?; [ $ret = 5 ] && exit 0 || exit $ret
