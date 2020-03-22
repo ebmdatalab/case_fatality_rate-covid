655d2ee83a2d1b2e1fc92e891e55898432c42975
Seb Bacon <seb.bacon@gmail.com>
2020-03-22 21:09:52 +0000

    Attempt to debug weirdness
    In my docker, I can see the kernelspec:
    
    ```
    jupyter kernelspec list --json
    {
      "kernelspecs": {
        "ir": {
          "resource_dir": "/root/.local/share/jupyter/kernels/ir",
    ```
    
    In the docker build output in github we see
    
        [InstallKernelSpec] Installed kernelspec ir in /root/.local/share/jupyter/kernels/ir
    
    Yet trying to run the notebook gives us:
    
    ```
    2020-03-22T20:45:12.5958036Z   File "/root/.pyenv/versions/3.8.1/bin/jupyter-nbconvert", line 8, in <module>
    2020-03-22T20:45:12.5958250Z     sys.exit(main())
    ...
    2020-03-22T20:45:12.5970306Z   File "/root/.pyenv/versions/3.8.1/lib/python3.8/site-packages/jupyter_client/kernelspec.py", line 236, in get_kernel_spec
    2020-03-22T20:45:12.5970469Z     raise NoSuchKernel(kernel_name)
    2020-03-22T20:45:12.5970624Z jupyter_client.kernelspec.NoSuchKernel: No such kernel named ir
    2020-03-22T20:45:12.8168598Z Cleaning up orphan processes
    ```

--- setup2.R
+++ setup2.R
@@ -4,7 +4,8 @@
 devtools::install_version("IRkernel")
 IRkernel::installspec()
 
-devtools::install_version("rvest", version = "0.3.5", repos = "http://cran.us.r-project.org")
-devtools::install_version("data.table", version = "1.12.8", repos = "http://cran.us.r-project.org")
-devtools::install_version("stringr", version = "1.4.0", repos = "http://cran.us.r-project.org")
-devtools::install_version("meta", version = "4.11-0", repos = "http://cran.us.r-project.org")
+#devtools::install_version("rvest", version = "0.3.5", repos = "http://cran.us.r-project.org")
+#devtools::install_version("data.table", version = "1.12.8", repos = "http://cran.us.r-project.org")
+#devtools::install_version("stringr", version = "1.4.0", repos = "http://cran.us.r-project.org")
+#devtools::install_version("meta", version = "4.11-0", repos = "http://cran.us.r-project.org")
+###
